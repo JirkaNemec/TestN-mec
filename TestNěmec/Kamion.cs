e70ffa09b344fd61c0183ddd477308aa88d0fe67
Jiří Němec <Nemec.Jiri@stud.vassboskovice.cz>
2022-05-17 11:25:25 +0200

    Přidejte soubory projektu.

---
+++ TestNěmec/Kamion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNěmec
+{
+    internal class Kamion
+    {
+        public int maxcarry { get; }
+        public int maxfuel { get; }
+        public int consum { get; }
+        public int distance { get; }
+        public int currentcarry { get; set; }
+        public int currentfuel { get; set; }
+
+        public Kamion(int nosnost, int nadrz, int spotreba)
+        {
+            maxfuel = nadrz;
+            consum = spotreba;
+            maxcarry = nosnost;
+            distance = 0;
+            currentcarry = 0;
+            currentfuel = 0;
+        }
+        public void Jet(int distance)
+        {
+            
+        }
+        
+    }
+}
---
+++ TestNěmec/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestNěmec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Jet(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            int m = 0;
+            try
+            {
+                m = int.Parse(Distance.Text);
+                Distance.Text = m.ToString();
+            }
+            catch
+            {
+                Distance.Text += 10;
+            }
+        }
+
+        private void plusko_Click(object sender, RoutedEventArgs e)
+        {
+            int m = 0;
+            try
+            {
+                m = int.Parse(Distance.Text);
+                Distance.Text = m.ToString();
+            }
+            catch
+            {
+                Distance.Text += 20;
+            }
+        }
+    }
+}
